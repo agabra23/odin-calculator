c7bfc7397d4f85ce1f409a8b563847a899fa934c
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-02-25 17:31:25 -0500

    Setup basic operator functions

---
+++ app.js
@@ -0,0 +1,16 @@
+function add() {
+    return arguments[0] + arguments[1];
+}
+
+function subtract() {
+    return arguments[0] - arguments[1];
+}
+
+function multiply() {
+    return arguments[0] * arguments[1];
+}
+
+function divide() {
+    return arguments[0] / arguments[1];
+}
+
