bf47f1e0a733ab3e8064795baa641aa4687a29bd
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-02-28 19:12:15 -0500

    Init variables and basic functions
    Still need to add event listeners

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
+const DEFAULT_NUM = undefined;
+const DEFAULT_OPERATOR = undefined;
+
+let num1, num2 = DEFAULT_NUM;
+let currentOperator = DEFAULT_OPERATOR;
+
+function setCurrentOperator(newOperator) {
+    currentOperator = newOperator;
+}
+
+const btn1 = document.querySelector('#1');
+const btn2 = document.querySelector('#2');
+const btn3 = document.querySelector('#3');
+const btn4 = document.querySelector('#4');
+const btn5 = document.querySelector('#5');
+const btn6 = document.querySelector('#6');
+const btn7 = document.querySelector('#7');
+const btn8 = document.querySelector('#8');
+const btn9 = document.querySelector('#9');
+const btn0 = document.querySelector('#0');
+
+const digits = document.querySelectorAll('.digit')
+
+const btnClear = document.querySelector('#clear');
+const btnInverse = document.querySelector('#inverse');
+const btnPercent = document.querySelector('#%');
+
+const btnPlus = document.querySelector('#+');
+const btnMinus = document.querySelector('#-');
+const btnMultiply = document.querySelector('#*');
+const btnDivide = document.querySelector('#/');
+const btnDecimal = document.querySelector('#decimalBtn');
+
+const operators = document.querySelectorAll('.operator')
+
+const display = document.querySelector('#displayText')
+
+
+
+
+
 function add() {
     return arguments[0] + arguments[1];
 }
@@ -35,6 +76,43 @@ function operate(num1, num2, operator) {
     }
 }
 
+function init() {
+    display.textContent = '';
+    num1, num2 = DEFAULT_NUM;
+    currentOperator = DEFAULT_OPERATOR;
+}
+
+function reset() {
+    init();
+}
+
+function activateButton(newOperator) {
+    if (currentOperator === '+') {
+        btnPlus.classList.remove('active');
+    } else if (currentMode === '-') {
+        btnMinus.classList.remove('active');
+    } else if (currentMode === '*') {
+        btnMultiply.classList.remove('active');
+    } else if (currentMode === '/') {
+        btnDivide.classList.remove('active');
+    }
+
+    if (newOperator === '+') {
+        btnPlus.classList.add('active');
+    } else if (newMode === '-') {
+        btnMinus.classList.add('active');
+    } else if (newMode === '*') {
+        btnMultiply.classList.add('active');
+    }else if (newMode === '/') {
+        btnDivide.classList.add('active');
+    }
+}
+
+function concatDisplay(digit) {
+    display.textContent += digit;
+}
+
+Window.onload = init();
 
 /* 
 Notes: String together operations
@@ -45,7 +123,7 @@ Notes: 'Clear' button completely clears data and resets the calculator
 Notes: Display 'dumbass' when dividing by 0
 Notes: Add keyboard Support
 Notes: add decimal support
-start with num1 = undefined, num2 = undefined
+*start with num1 = undefined, num2 = undefined
 Concat digit with result text box each digit click
 save first and second num (same as first) when operator is clicked
 next digit to be clicked is concat to result text box
